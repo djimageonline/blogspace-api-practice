a6f8202a4dac523e31d092e73a6b4fdb25db21ff
Johnny Proano <jproano22@yahoo.com>
2023-04-26 15:11:55 -0400

    refacoterd code to add array and new method and form.reset

--- index.js
+++ index.js
@@ -1,60 +1,14 @@
-fetch("https://apis.scrimba.com/jsonplaceholder/posts")
-  .then((res) => res.json())
-  .then((data) => {
-    const postsArr = data.slice(0, 5);
-    let html = "";
-    for (let post of postsArr) {
-      html += `
-                <h3>${post.title}</h3>
-                <p>${post.body}</p>
-                <hr />
-            `;
-    }
-    document.getElementById("blog-list").innerHTML = html;
-  });
-
-document.getElementById("new-post").addEventListener("submit", function (e) {
-  e.preventDefault();
-  const postTitle = document.getElementById("post-title").value;
-  const postBody = document.getElementById("post-body").value;
-  const data = {
-    title: postTitle,
-    body: postBody,
-  };
-
-  const options = {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
-
-  fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
-    .then((res) => res.json())
-    .then((post) => {
-      console.log(post);
-
-      let newHtml = `
-        <h3>${post.title}</h3>
-        <p>${post.body}</p>
-        <hr />
-        ${document.getElementById("blog-list").innerHTML}
-      `;
-
-      document.getElementById("blog-list").innerHTML = newHtml;
-    });
-});
 let postsArray = [];
+const form = document.getElementById("new-post");
 
 function renderPosts() {
   let html = "";
   for (let post of postsArray) {
     html += `
-                <h3>${post.title}</h3>
-                <p>${post.body}</p>
-                <hr />
-            `;
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>
+            <hr />
+        `;
   }
   document.getElementById("blog-list").innerHTML = html;
 }
@@ -66,7 +20,7 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     renderPosts();
   });
 
-document.getElementById("new-post").addEventListener("submit", function (e) {
+form.addEventListener("submit", function (e) {
   e.preventDefault();
   const postTitle = document.getElementById("post-title").value;
   const postBody = document.getElementById("post-body").value;
@@ -88,5 +42,6 @@ document.getElementById("new-post").addEventListener("submit", function (e) {
     .then((post) => {
       postsArray.unshift(post);
       renderPosts();
+      form.reset();
     });
 });
